8e2f010940185902bb187b68c280bef02d2b29ab
dheeman pati <dheeman.p@simformsolutions.com>
2023-06-30 13:24:00 +0530

    fix: ediotr js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Editor from "vue-editor-js/src/index";
-
 Vue.config.productionTip = false;
-Vue.use(Editor);
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
