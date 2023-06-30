f0773378a6c0bf2a9c679c951a1e38741e1865e6
dheeman pati <dheeman.p@simformsolutions.com>
2023-06-30 12:45:19 +0530

    Feat:added editor

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Editor from "vue-editor-js/src/index";
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.use(Editor);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
